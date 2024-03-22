8173bc8d717ce6478a03beb1ced3d55faa436b16
Nikita Soloviev <Nikita134679@gmail.com>
2024-03-23 01:11:04 +0300

    first commit book-reader

---
+++ dom/book-reader/task.js
@@ -0,0 +1,22 @@
+const fontSize = Array.from(document.querySelectorAll(".font-size"));
+const bookContent = document.querySelector(".book__content");
+
+fontSize.forEach((element, index) => {
+    element.addEventListener("click", item => {
+        fontSize.forEach(item => {
+            item.classList.remove("font-size_active");
+        });
+        item.preventDefault();
+        element.classList.add("font-size_active");
+        if (index === 0) {
+            bookContent.classList.add("book_fs-small");
+        };
+        if (index === 1) {
+            bookContent.classList.remove("book_fs-small");
+        };
+        if (index === 2) {
+            bookContent.classList.add("book_fs-big");
+        };
+    });
+    
+});
