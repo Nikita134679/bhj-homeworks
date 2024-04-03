d64f260bbe7914c9d6ebd1b64976e6deedec4d7b
Nikita Soloviev <Nikita134679@gmail.com>
2024-04-03 03:26:55 +0300

    second commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,13 +1,26 @@
 const hasToolTip = Array.from(document.querySelectorAll(".has-tooltip"));
-const body = document.querySelector("body");
+const body = document.getElementsByClassName("body");
+const toolTip = document.createElement("div");
+toolTip.classList.add("tooltip_active");
+toolTip.classList.add("tooltip");
+toolTip.setAttribute('style', 'left: 0; top: 0;')
+
+
 
 hasToolTip.forEach((element, index) => {
     element.addEventListener("click", event => {
-    event.preventDefault();
-    let div = document.createElement("div");
-    hasToolTip[index].insertAdjacentHTML("afterEnd", '<div class="tooltip"></div>');
-    const toolTip = document.querySelector(".tooltip");
-    toolTip.textContent = hasToolTip[index].title;
-    toolTip.classList.add("tooltip_active")
+        event.preventDefault();
+        if (element.previousElementSibling === toolTip) {
+            toolTip.classList.toggle("tooltip_active");
+        } else {
+            hasToolTip[index].parentNode.insertBefore(toolTip, hasToolTip[index]);
+            toolTip.textContent = hasToolTip[index].title;
+        }
+        let position = hasToolTip[index].getBoundingClientRect()
+        let positionTop = Math.ceil(`${position.y}`)
+        toolTip.style.top = `${positionTop + 25}px`
+
+        let positionleft = Math.ceil(`${position.x}`)
+        toolTip.style.left = `${positionleft + 5}px`
     });
 });
