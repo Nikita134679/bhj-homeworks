bdc526d45647408c50fa66d19d93aa87f262bd1a
Nikita Soloviev <Nikita134679@gmail.com>
2024-04-08 01:05:26 +0300

    first commit poll

---
+++ async-requests/poll/task.js
@@ -0,0 +1,28 @@
+const xhr = new XMLHttpRequest();
+const item = document.getElementById("poll__title");
+const poll = document.getElementById("poll__answers");
+const pollAnswer = document.getElementsByClassName("poll__answers");
+
+xhr.addEventListener("readystatechange", () => {
+    if (xhr.readyState === xhr.DONE) {
+        obj = JSON.parse(xhr.response);
+        console.log(obj);
+
+        item.textContent = obj.data.title;
+        let answersLength = obj.data.answers.length;
+        
+        for (let i = 0; i < answersLength; i++) {
+            poll.insertAdjacentHTML("beforeEnd",`<button class="poll__answer"> ${obj.data.answers[i]} </button>`);
+        };
+
+        Array.from(pollAnswer).forEach((element, index) => {
+            element.addEventListener("click", event => {
+                alert("Спасибо, ваш голос засчитан!");
+            });
+        });
+    };
+});
+
+xhr.open("GET", "https://students.netoservices.ru/nestjs-backend/poll");
+
+xhr.send();
