3095511bfcad0ff75ff2a206a9b9fe48208c2e23
Nikita Soloviev <Nikita134679@gmail.com>
2024-04-09 02:02:40 +0300

    second commit preloader

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -2,17 +2,15 @@ const xhr = new XMLHttpRequest();
 const items = document.getElementById("items");
 const item = document.getElementsByClassName("item");
 const removeImage = document.getElementById("loader");
-let obj;
 let value = [];
 let charCode = [];
 
-xhr.addEventListener("readystatechange", () => {
-  if (xhr.readyState === xhr.DONE) {
+xhr.addEventListener("load", () => {
     removeImage.classList.remove("loader_active");
-    obj = JSON.parse(xhr.response);
+    let obj = JSON.parse(xhr.response);
     for (let key in obj.response.Valute) {
-        value.push(obj.response.Valute[key].Value);
-        charCode.push(obj.response.Valute[key].CharCode);
+      value.push(obj.response.Valute[key].Value);
+      charCode.push(obj.response.Valute[key].CharCode);
     };
 
     for (let i = 0; i < value.length; i++) {
@@ -20,9 +18,8 @@ xhr.addEventListener("readystatechange", () => {
         item[i].insertAdjacentHTML("beforeEnd",`<div class="item__code">${charCode[i]}</div>`);
         item[i].insertAdjacentHTML("beforeEnd",`<div class="item__value">${value[i]}</div>`);
         item[i].insertAdjacentHTML("beforeEnd",`<div class="item__value">руб.</div>`);
-    }
-  }
-})
+    };
+});
 
 xhr.open("GET", "https://students.netoservices.ru/nestjs-backend/slow-get-courses");
 
