15bc82f0cb85aa2248b82874b2a0ebfb6724aa8a
Nikita Soloviev <Nikita134679@gmail.com>
2024-04-09 02:04:33 +0300

    first commit poll

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -3,24 +3,22 @@ const item = document.getElementById("poll__title");
 const poll = document.getElementById("poll__answers");
 const pollAnswer = document.getElementsByClassName("poll__answers");
 
-xhr.addEventListener("readystatechange", () => {
-    if (xhr.readyState === xhr.DONE) {
-        obj = JSON.parse(xhr.response);
-        console.log(obj);
+xhr.addEventListener("load", () => {
+    obj = JSON.parse(xhr.response);
+    console.log(obj);
 
-        item.textContent = obj.data.title;
-        let answersLength = obj.data.answers.length;
-        
-        for (let i = 0; i < answersLength; i++) {
-            poll.insertAdjacentHTML("beforeEnd",`<button class="poll__answer"> ${obj.data.answers[i]} </button>`);
-        };
-
-        Array.from(pollAnswer).forEach((element, index) => {
-            element.addEventListener("click", event => {
-                alert("Спасибо, ваш голос засчитан!");
-            });
-        });
+    item.textContent = obj.data.title;
+    let answersLength = obj.data.answers.length;
+    
+    for (let i = 0; i < answersLength; i++) {
+        poll.insertAdjacentHTML("beforeEnd",`<button class="poll__answer"> ${obj.data.answers[i]} </button>`);
     };
+
+    Array.from(pollAnswer).forEach((element, index) => {
+        element.addEventListener("click", event => {
+            alert("Спасибо, ваш голос засчитан!");
+        });
+    });
 });
 
 xhr.open("GET", "https://students.netoservices.ru/nestjs-backend/poll");
