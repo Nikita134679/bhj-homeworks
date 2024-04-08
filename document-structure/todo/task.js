5f06209e6ae35fea20e33151c31cc96776320a24
Nikita Soloviev <Nikita134679@gmail.com>
2024-04-09 01:46:48 +0300

    third commit todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,36 +1,26 @@
 const form = document.getElementById("tasks__form");
 const tasksList = document.getElementById("tasks__list");
 const input = document.getElementById("task__input");
-const link = document.getElementsByClassName("task__remove");
-const task = document.getElementsByClassName("task");
 
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    if (input.value.trim() != 0) {
-
-        let divTask = document.createElement("div");
-        tasksList.appendChild(divTask);
-        divTask.classList.add("task");
-
-        let divTaskTitle = document.createElement("div");
-        divTask.appendChild(divTaskTitle);
-        divTaskTitle.classList.add("task__title");
-        divTaskTitle.textContent = input.value;
-
-        divTaskTitle.insertAdjacentHTML("afterEnd", '<a href="#" class="task__remove">&times;</a>');
-
-
-        form.reset();
-
-    } else {
-        alert("Поле не должго быть пустым!");
+    if (input.value.trim().length === 0) {
+        return alert("Поле не должго быть пустым!");
     };
 
-    Array.from(link).forEach(e => {
-        e.addEventListener("click", event => {
-            e.parentNode.remove()
-        })
+    const task = document.createElement("div");
+    tasksList.appendChild(task)
+    task.classList.add("task");
+    task.insertAdjacentHTML("beforeEnd",`<div class="task__title">${input.value}</div>`);
+    task.insertAdjacentHTML("beforeEnd", '<a href="#" class="task__remove">&times;</a>');
+
+    let remove = task.querySelector(".task__remove");
+    remove.addEventListener("click", event => {
+        task.remove();
     });
+
+    form.reset();
+
 });
